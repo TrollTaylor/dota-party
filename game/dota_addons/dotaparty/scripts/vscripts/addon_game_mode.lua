55b8d05fde36d0e1e72bd4779b99addadb964a8d
Taylor Troll <taylordude145@gmail.com>
2015-11-02 23:28:42 -0600

    no message

--- game/dota_addons/dotaparty/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/dotaparty/scripts/vscripts/addon_game_mode.lua
@@ -27,6 +27,12 @@ function DotaParty:OnNPCSpawned( keys )
 
 	local npc = EntIndexToHScript(keys.entindex)
 
+	if npc:IsRealHero() then
+		playersSpaces[PlayerResource:GetPlayer(npc:GetPlayerOwnerID())] = 1
+
+	end
+
+
 	if  npc:GetUnitName() == "npc_dota_blue_space" then
 		npc:SetRenderColor(0,0,1000)
 		space = npc
@@ -47,29 +53,44 @@ end
 
 function RollDice(eventSourceIndex, args)	
 	local player = PlayerResource:GetPlayer(args['roller']) 
-	local space = FindUnitsInRadius(DOTA_TEAM_NEUTRALS, PlayerResource:GetSelectedHeroEntity(args['roller']):GetOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_CREEP, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
+	local space = FindUnitsInRadius(DOTA_TEAM_NEUTRALS, PlayerResource:GetSelectedHeroEntity(args['roller']):GetOrigin(), nil, 100000, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_CREEP, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
 	local roll =  math.random(1,10)
+	local spaceimgoingto = nil
 	print("roll = ", roll)
-	playersSpaces[player] = 1
-	for i = 1, roll do 
+				
+					for i = 1, roll do 
 
 		--print(space[i]:GetOrigin())
-		print(space[i]:GetDebugName())
 				--print(space[i]:GetDebugName(), " equaled I")
-				
-					print("yeye")
- 		local order = {
+				for a = 1, tablelength(space) do
+        spaceimgoingto =  a
+
+					if tonumber(space[a]:GetDebugName()) == i  then
+											print("a = ", space[a]:GetDebugName(), " i = ", space[i]:GetDebugName(), " playersSpaces = ", playersSpaces[player])
+
+	local order = {
             UnitIndex = PlayerResource:GetSelectedHeroEntity(args['roller']):GetEntityIndex(),
             OrderType = DOTA_UNIT_ORDER_MOVE_TO_POSITION,
-            Position = space[playersSpaces[player] + i]:GetOrigin(),
+            Position = space[ spaceimgoingto  ]:GetOrigin(),
             Queue = true
         }
-        ExecuteOrderFromTable(order)
-				playersSpaces[player] = playersSpaces[player] + roll
-				print("space = ", playersSpaces[player])
+        ExecuteOrderFromTable(order)	
+					end 
+				end
+					--print("yeye")
+ 	
 		
 		
 	end
+
+				playersSpaces[player] = playersSpaces[player] + roll
+				print("space = ", playersSpaces[player])
+        print("the space im going to is ", spaceimgoingto)
+
+end
+
+function setPlayerSpace(playeSpce)
+	playersSpaces[playeSpce] = 1
 end
 
 function DotaParty:InitGameMode()
@@ -83,6 +104,25 @@ function DotaParty:InitGameMode()
 
 
 
+Convars:RegisterCommand( "playerID", function()
+
+    local cmdPlayer = Convars:GetCommandClient()
+    local player = PlayerResource:GetPlayer(cmdPlayer:GetPlayerID())
+
+    if cmdPlayer then
+        return setPlayerSpace(player)
+    end
+end, "a", 0 )
+
+Convars:RegisterCommand( "playerIDp", function()
+
+    local cmdPlayer = Convars:GetCommandClient()
+    local player = PlayerResource:GetPlayer(cmdPlayer:GetPlayerID())
+
+    if cmdPlayer then
+        return print(playersSpaces[player])
+    end
+end, "a", 0 )
 
 
 end
